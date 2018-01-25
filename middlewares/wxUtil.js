134c4555eb38b75d25c4c2f425ffccce7ace9781
吴金锋 <jinfeng.wu@edaibu.net>
2018-01-25 21:19:52 +0800

    添加翻译接口回复

--- middlewares/wxUtil.js
+++ middlewares/wxUtil.js
@@ -25,15 +25,26 @@ let messageText = function (resultXmlObj) {
     let returnContent = resultXmlObj.Content;
     if (resultXmlObj.Content === '1') {
         returnContent = '你好';
+        return returnTextXml(resultXmlObj, returnContent)
     } else if (resultXmlObj.Content === '2') {
         return messageNews(resultXmlObj);
     } else if (resultXmlObj.Content === '3') {
         return messageImage(resultXmlObj);
     } else if (resultXmlObj.Content === '?' || resultXmlObj.Content === '？') {
-        returnContent = '请回复1，2，3?';
+        returnContent = '请回复1，2，3， 翻译XXX， ?';
+        return returnTextXml(resultXmlObj, returnContent)
+    } else if (/^翻译/.test(resultXmlObj.Content)) {
+        // 异步的返回不成功
+        returnContent = returnContent.replace(/^翻译/, '');
+        let content = translate(returnContent);
+        return returnTextXml(resultXmlObj, content);
     } else {
-        returnContent = '请回复1，2，3?';
+        returnContent = '请回复1，2，3，翻译XXX，3?';
+        return returnTextXml(resultXmlObj, returnContent)
     }
+};
+
+let returnTextXml = function (resultXmlObj, returnContent) {
     let obj = {
         ToUserName: resultXmlObj.FromUserName,
         FromUserName: resultXmlObj.ToUserName,
@@ -88,9 +99,9 @@ let messageImage = function (resultXmlObj) {
     let obj = {
         ToUserName: resultXmlObj.FromUserName,
         FromUserName: resultXmlObj.ToUserName,
-        CreateTime: Math.round(new Date().getTime()/1000),
+        CreateTime: Math.round(new Date().getTime() / 1000),
         MsgType: 'image',
-        Image:{
+        Image: {
             MediaId: '8d3qCFs_yHW__HDcZFS4hJLoQ6iRU5Et86X2cNcslvz28iJHvGcPtLMBOoPDPjFw'
         }
     };
@@ -146,7 +157,7 @@ let mediaUploadTemp = function () {
         // Pass data via Buffers
         my_buffer: new Buffer([1, 2, 3]),
         // Pass data via Streams
-        my_file: fs.createReadStream(path.join(__dirname , '../public/img/imooc.png'))
+        my_file: fs.createReadStream(path.join(__dirname, '../public/img/imooc.png'))
     };
     request.post({url: url, formData: formData}, function optionalCallback(err, httpResponse, body) {
         if (err) {
@@ -160,45 +171,45 @@ let mediaUploadTemp = function () {
 // 组装 菜单
 let initMenu = function () {
     let clickBtn11 = {
-        "type":"click",
-        "name":"今日歌曲",
-        "key":"11"
+        "type": "click",
+        "name": "今日歌曲",
+        "key": "11"
     };
     let viewBtn21 = {
-        "type":"view",
-        "name":"vie搜索",
-        "url":"http://www.soso.com/"
+        "type": "view",
+        "name": "vie搜索",
+        "url": "http://www.soso.com/"
     };
     let clickBtn31 = {
-        "type":"scancode_push",
-        "name":"扫码事件",
-        "key":"31",
+        "type": "scancode_push",
+        "name": "扫码事件",
+        "key": "31",
     };
     let clickBtn32 = {
-        "type":"location_select",
-        "name":"地理位置",
-        "key":"32",
+        "type": "location_select",
+        "name": "地理位置",
+        "key": "32",
     };
-    let btn3 =  {
-        "name":"菜单",
-        "sub_button":[clickBtn31, clickBtn32]
+    let btn3 = {
+        "name": "菜单",
+        "sub_button": [clickBtn31, clickBtn32]
     };
 
-    let menu =  {button:[clickBtn11,viewBtn21,btn3]};
+    let menu = {button: [clickBtn11, viewBtn21, btn3]};
     return JSON.stringify(menu);
 };
 
-let createMenu = function(token, menu){
-    let url = wxConfig.createMenuURL+token;
-    comm.postRequest(url, menu, (err, doc)=>{
-        if(err){
+let createMenu = function (token, menu) {
+    let url = wxConfig.createMenuURL + token;
+    comm.postRequest(url, menu, (err, doc) => {
+        if (err) {
             console.log('创建菜单出错');
             console.log(err);
-        }else{
-            if(doc.errcode === 0){
+        } else {
+            if (doc.errcode === 0) {
                 console.log('创建菜单成功')
-            }else{
-                console.log('创建菜单失败'+doc.errcode+','+doc.errmsg);
+            } else {
+                console.log('创建菜单失败' + doc.errcode + ',' + doc.errmsg);
             }
         }
     });
@@ -249,10 +260,10 @@ _getAccessToken((err, obj) => {
         logger.info('程序启动获取_accessToken:ok');
         console.log(_accessToken)
         _timeoutRefresh();
-       // mediaUploadTemp();
-       // createMenu(_accessToken.access_token, initMenu())
-       // queryMenu(_accessToken.access_token)
-       // deleteMenu(_accessToken.access_token)
+        // mediaUploadTemp();
+        // createMenu(_accessToken.access_token, initMenu())
+        // queryMenu(_accessToken.access_token)
+        // deleteMenu(_accessToken.access_token)
     }
 });
 
@@ -261,7 +272,7 @@ let myToken = function () {
     return _accessToken.access_token;
 };
 // 视图url跳转信息
-let messageView = function (resultXmlObj,url) {
+let messageView = function (resultXmlObj, url) {
     let obj = {
         ToUserName: resultXmlObj.FromUserName,
         FromUserName: resultXmlObj.ToUserName,
@@ -274,7 +285,7 @@ let messageView = function (resultXmlObj,url) {
     return xml;
 };
 // 扫码信息
-let messageScancode = function (resultXmlObj,key) {
+let messageScancode = function (resultXmlObj, key) {
     let obj = {
         ToUserName: resultXmlObj.FromUserName,
         FromUserName: resultXmlObj.ToUserName,
@@ -287,7 +298,7 @@ let messageScancode = function (resultXmlObj,key) {
     return xml;
 };
 // 位置信息
-let messageLocation = function (resultXmlObj, label){
+let messageLocation = function (resultXmlObj, label) {
     let obj = {
         ToUserName: resultXmlObj.FromUserName,
         FromUserName: resultXmlObj.ToUserName,
@@ -301,13 +312,13 @@ let messageLocation = function (resultXmlObj, label){
 };
 
 // 查询菜单
-let queryMenu= function(token){
-    let url = wxConfig.getMenuURL+token;
-    comm.getRequest(url, (err, doc)=>{
-        if(err){
+let queryMenu = function (token) {
+    let url = wxConfig.getMenuURL + token;
+    comm.getRequest(url, (err, doc) => {
+        if (err) {
             console.log('查询菜单出错');
             console.log(err);
-        }else{
+        } else {
             console.log('菜单')
             console.log(JSON.stringify(doc))
         }
@@ -315,17 +326,17 @@ let queryMenu= function(token){
 };
 
 // 删除菜单
-let deleteMenu = function(token){
-    let url = wxConfig.delMenuURL+token;
-    comm.getRequest(url, (err, doc)=>{
-        if(err){
+let deleteMenu = function (token) {
+    let url = wxConfig.delMenuURL + token;
+    comm.getRequest(url, (err, doc) => {
+        if (err) {
             console.log('删除菜单出错');
             console.log(err);
-        }else{
-            if(doc.errcode === 0){
+        } else {
+            if (doc.errcode === 0) {
                 console.log('删除菜单成功')
-            }else{
-                console.log('删除菜单失败'+doc.errcode+','+doc.errmsg);
+            } else {
+                console.log('删除菜单失败' + doc.errcode + ',' + doc.errmsg);
             }
         }
     });
@@ -333,7 +344,7 @@ let deleteMenu = function(token){
 
 
 // 百度翻译接口
-let translate = function (source) {
+let translate = function (source, cb) {
     let id = wxConfig.baiduAppId;
     let secret = wxConfig.baiduSecret;
     let q = source;
@@ -341,31 +352,28 @@ let translate = function (source) {
     let to = 'zh';
     let salt = (new Date).getTime();
     let str1 = id + q + salt + secret;
-    console.log(str1)
     let sign = comm.md5(str1);
     let url = `${wxConfig.baiduTranslateURL}?q=${encodeURIComponent(q)}&appid=${id}&salt=${salt}&from=${from}&to=${to}&sign=${sign}`;
     console.log(url);
-    comm.getRequest(url, (err, doc)=>{
-        if(err){
-            console.log('翻译出错');
-            console.log(err);
-        }else{
-            console.log(doc)
-            if(doc.error_code){
-                console.log('翻译出错'+doc.error_code+doc.error_msg);
-            }else{
-                console.log('翻译ok');
-                let trans_result = doc.trans_result;
-                let dst = trans_result[0].dst;
-                console.log(dst)
+    return new Promise((resolve, reject) => {
+        comm.getRequest(url, (err, doc) => {
+            if (err) {
+                console.log('翻译出错');
+                reject(err);
+            } else {
+                console.log(doc)
+                if (doc.error_code) {
+                    reject('翻译出错' + doc.error_code + doc.error_msg)
+                } else {
+                    console.log('翻译ok');
+                    let trans_result = doc.trans_result;
+                    let dst = trans_result[0].dst;
+                    resolve(dst);
+                }
             }
-        }
-    })
+        })
+    });
 }
-// translate('app');
-
-
-
 
 
 module.exports = {
